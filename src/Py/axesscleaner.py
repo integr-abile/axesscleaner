18a7a2454b9e97de9d88c228407586db88248efd
Michele Berra <mikyberra@gmail.com>
2018-07-31 18:05:32 +0200

    Fixed bug and

--- src/Py/axesscleaner.py
+++ src/Py/axesscleaner.py
@@ -483,69 +483,72 @@ def multi_substitution_regexp(rexpr, textafter):
 
 
 # Begin of actual methods. First check if the input is a LaTex file
-if args.input.endswith('.tex'):
-    # Check the number of outputs. If no output is given, create a new one.
-    if not args.output:
-        a = args.input
-        args.output = a.replace('.tex', '_clean.tex')
-    # Assign the macro file address and some temporary files.
-    FOLDER_PATH = os.path.abspath(os.path.join(os.path.abspath(args.input), os.pardir))
-    MACRO_FILE = os.path.join(FOLDER_PATH, "user_macro.sty")
-    TEMP_FILE_PRE_EXPANSION = os.path.join(FOLDER_PATH, "temp_pre.tex")
+if args.input is not None:
+    if args.input.endswith('.tex'):
+        # Check the number of outputs. If no output is given, create a new one.
+        if not args.output:
+            a = args.input
+            args.output = a.replace('.tex', '_clean.tex')
+        # Assign the macro file address and some temporary files.
+        FOLDER_PATH = os.path.abspath(os.path.join(os.path.abspath(args.input), os.pardir))
+        MACRO_FILE = os.path.join(FOLDER_PATH, "user_macro.sty")
+        TEMP_FILE_PRE_EXPANSION = os.path.join(FOLDER_PATH, "temp_pre.tex")
 
-    # Reads the file preamble to obtain the user-defined macros. We also remove unwanted comments.
-    print("gather macros from preamble")
-    with open(args.input, 'r') as i:
-        line = strip_comments(i.read())
-        gather_macro(line)
-
-    # Reads user-macro file to obtain the user-defined macros. We also remove unwanted comments
-    print("gather macros from user defined file")
-    if os.path.exists(MACRO_FILE):
-        with open(MACRO_FILE, 'r') as i:
+        # Reads the file preamble to obtain the user-defined macros. We also remove unwanted comments.
+        print("gather macros from preamble")
+        with open(args.input, 'r') as i:
             line = strip_comments(i.read())
             gather_macro(line)
 
-    # Remove the macros from the main file and writes the output to a temp file.
-    print("remove macros from main file")
-    with open(args.input, 'r') as i:
-        line = strip_comments(i.read())
-        remove_macro(line, TEMP_FILE_PRE_EXPANSION)
+        # Reads user-macro file to obtain the user-defined macros. We also remove unwanted comments
+        print("gather macros from user defined file")
+        if os.path.exists(MACRO_FILE):
+            with open(MACRO_FILE, 'r') as i:
+                line = strip_comments(i.read())
+                gather_macro(line)
 
-    # Get path of temp file.
-    current_path = os.path.split(TEMP_FILE_PRE_EXPANSION)[0]
+        # Remove the macros from the main file and writes the output to a temp file.
+        print("remove macros from main file")
+        with open(args.input, 'r') as i:
+            line = strip_comments(i.read())
+            remove_macro(line, TEMP_FILE_PRE_EXPANSION)
 
-    # Include all the external files
-    print("include external files in main file")
-    final_text_to_expand = strip_comments(''.join(expand_file(TEMP_FILE_PRE_EXPANSION, current_path, True, False)))
+        # Get path of temp file.
+        current_path = os.path.split(TEMP_FILE_PRE_EXPANSION)[0]
 
-    # Remove temp file
-    os.remove(TEMP_FILE_PRE_EXPANSION)
+        # Include all the external files
+        print("include external files in main file")
+        final_text_to_expand = strip_comments(''.join(expand_file(TEMP_FILE_PRE_EXPANSION, current_path, True, False)))
 
-    # Remove macros from the entire file and put the result to temp file
-    print("remove macros from entire file")
-    remove_macro(final_text_to_expand, TEMP_FILE_PRE_EXPANSION)
+        # Remove temp file
+        os.remove(TEMP_FILE_PRE_EXPANSION)
 
-    # get script folder
-    script_path = os.path.abspath(os.path.join(__file__, os.pardir))
-    preprocess_path = os.path.join(script_path, "..", "Perl", "AxessibilityPreprocess.pl")
-    preprocess_compile_path = os.path.join(script_path, "..", "Perl", "AxessibilityPreprocesspdfLatex.pl")
+        # Remove macros from the entire file and put the result to temp file
+        print("remove macros from entire file")
+        remove_macro(final_text_to_expand, TEMP_FILE_PRE_EXPANSION)
 
-    # Call perl scripts to clean dollars, underscores. Eventually, it can call also pdflatex, when -p is selected
-    if args.pdflatex:
-        print("final cleaning file")
-        p = subprocess.Popen(
-            ["perl", preprocess_compile_path, "-w", "-o", "-s", TEMP_FILE_PRE_EXPANSION, args.output])
+        # get script folder
+        script_path = os.path.abspath(os.path.join(__file__, os.pardir))
+        preprocess_path = os.path.join(script_path, "..", "Perl", "AxessibilityPreprocess.pl")
+        preprocess_compile_path = os.path.join(script_path, "..", "Perl", "AxessibilityPreprocesspdfLatex.pl")
+
+        # Call perl scripts to clean dollars, underscores. Eventually, it can call also pdflatex, when -p is selected
+        if args.pdflatex:
+            print("final cleaning file")
+            p = subprocess.Popen(
+                ["perl", preprocess_compile_path, "-w", "-o", "-s", TEMP_FILE_PRE_EXPANSION, args.output])
+        else:
+            print("final cleaning file and pdf production")
+            p = subprocess.Popen(
+                ["perl", preprocess_path, "-w", "-o", "-s", TEMP_FILE_PRE_EXPANSION, args.output])
+
+        # close process.
+        p.communicate()
+
+        # remove spurious file
+        os.remove(TEMP_FILE_PRE_EXPANSION)
+        os.remove(TEMP_FILE_PRE_EXPANSION.replace('.tex', '.bak'))
     else:
-        print("final cleaning file and pdf production")
-        p = subprocess.Popen(
-            ["perl", preprocess_path, "-w", "-o", "-s", TEMP_FILE_PRE_EXPANSION, args.output])
-
-    # close process.
-    p.communicate()
-
-    # remove spurious file
-    os.remove(TEMP_FILE_PRE_EXPANSION)
-    os.remove(TEMP_FILE_PRE_EXPANSION.replace('.tex', '.bak'))
+        print('The file you inserted as input is not a .tex')
 else:
-    print('The file you inserted as input is not a .tex')
+    print('The input is empty')
